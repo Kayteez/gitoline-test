ed1defda02581b5633d90859d2398a32683a7407
Tuan Kiet <kiet972002@gmail.com>
2023-05-17 10:46:59 +0700

    Component part 2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navigation from './components/Navigation';
+import Player from './Players';
+import Footer from './Footer';
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Player/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
