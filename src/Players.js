fe555903469364efd4c4ecddd599db5a0f4920b2
Tuan Kiet <kiet972002@gmail.com>
2023-05-17 11:19:47 +0700

    Functional Components

--- src/Players.js
+++ src/Players.js
@@ -57,6 +57,24 @@
 //         )
 //                 }
 // }
+// import React from "react";
+// import { Players } from "./shared/ListOfPlayers";
+// export default function Player() {
+//         return(
+//             <div className="container">
+//                 {Players.map((player)=>(
+//                     <div className="column">
+//                     <div className="card">
+//                         <img src={process.env.PUBLIC_URL+player.img}/>
+//                         <h3>{player.name}</h3>
+//                         <p className="title">{player.club}</p>
+//                         <p><button>Detail</button></p>
+//                     </div>
+//                 </div>
+//                 ))}
+//             </div>
+//         )
+// }
 import React from "react";
 import { Players } from "./shared/ListOfPlayers";
 export default function Player() {
